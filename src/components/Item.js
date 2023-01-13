5d49adb8c714cf5400513fbb00994bc6a7355abf
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-13 22:27:28 +0300

    Add React filter and add functionality.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleCart () {
+    setInCart((inCart) => !inCart);
+  }
+
+  const liClass = inCart ? "in-cart" : "";
+  const btnClass = inCart ? "remove" : "add";
   return (
-    <li className="">
-      <span>{name}</span>
+    <li className={liClass}>
+      <span> {name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={btnClass} onClick={handleCart}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
